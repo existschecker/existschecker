0a0b4623ee1b0ba17dd002495ccbada007479bc2
check-your-proofs <233064824+check-your-proofs@users.noreply.github.com>
2025-10-18 20:46:56 +0900

    Remove functions for logic_equiv() and EGraph

--- proofsrc/logic_utils.py
+++ proofsrc/logic_utils.py
@@ -1,15 +1,6 @@
 from ast_types import Or, Not, Forall, Exists, ExistsUniq, Implies, Iff, And, Symbol, Context, Compound, Fun, Con, Var, Bottom, Term, Pred, pretty_expr
 from itertools import permutations
-from dataclasses import dataclass
 from copy import deepcopy
-
-@dataclass
-class BoolTrue:
-    pass
-
-@dataclass
-class BoolFalse:
-    pass
 
 def flatten_op(expr, op: type[And] | type[Or]) -> list:
     if isinstance(expr, op):
@@ -118,12 +109,6 @@ def alpha_equiv(e1, e2, context: Context, env: dict[Var, Var] | None = None) -> 
     if isinstance(e1, Var) and isinstance(e2, Var):
         return env.get(e1, e1) == e2
 
-    if isinstance(e1, BoolTrue) and isinstance(e2, BoolTrue):
-        return True
-
-    if isinstance(e1, BoolFalse) and isinstance(e2, BoolFalse):
-        return True
-
     return False
 
 def collect_quantifier_vars(e, quantifier_type: type[Forall] | type[Exists]) -> tuple[list[Var], object]:
@@ -169,9 +154,6 @@ def collect_vars(expr, bound: set[Var] | None = None) -> tuple[set[Var], set[Var
     elif isinstance(expr, (Forall, Exists, ExistsUniq)):
         f_body, b_body = collect_vars(expr.body, bound | {expr.var})
         return f_body, b_body | {expr.var}
-
-    elif isinstance(expr, (BoolTrue, BoolFalse)):
-        return set(), set()
 
     else:
         raise Exception(f"Unexpected expr {pretty_expr(expr)}")
@@ -243,52 +225,6 @@ def normalize_neg(expr):
     else:
         raise Exception(f"Unexpected expr: {pretty_expr(expr)}")
 
-def to_core_logic_form(expr, context: Context, expand_all: bool = False):
-    if isinstance(expr, Symbol):
-        if expr.name in context.atoms:
-            return Symbol(expr.name, [to_core_logic_form(arg, context, expand_all) for arg in expr.args])
-        if expr.name in context.defpres:
-            defpre = context.defpres[expr.name]
-            if defpre.autoexpand or expand_all:
-                expanded = substitute(defpre.formula, dict(zip(defpre.args, expr.args)))
-                return to_core_logic_form(expanded, context, expand_all)
-            else:
-                return Symbol(expr.name, [to_core_logic_form(arg, context, expand_all) for arg in expr.args])
-        else:
-            raise Exception(f"Unexpected expr (Symbol): {pretty_expr(expr)}")
-    elif isinstance(expr, Compound):
-        if expr.fun.name in context.deffuns:
-            return Compound(to_core_logic_form(expr.fun, context, expand_all), [to_core_logic_form(arg, context, expand_all) for arg in expr.args])
-        elif expr.fun.name in context.deffunterms:
-            deffunterm = context.deffunterms[expr.fun.name]
-            if expand_all:
-                expanded = substitute(deffunterm.term, dict(zip(deffunterm.args, expr.args)))
-                return to_core_logic_form(expanded, context, expand_all)
-            else:
-                return Compound(to_core_logic_form(expr.fun, context, expand_all), [to_core_logic_form(arg, context, expand_all) for arg in expr.args])
-        else:
-            raise Exception(f"Unexpected expr (Compound): {pretty_expr(expr)}")
-    elif isinstance(expr, (Fun, Con, Var)):
-        return expr
-    elif isinstance(expr, Not):
-        return Not(to_core_logic_form(expr.body, context, expand_all))
-    elif isinstance(expr, (And, Or)):
-        return type(expr)(to_core_logic_form(expr.left, context, expand_all), to_core_logic_form(expr.right, context, expand_all))
-    elif isinstance(expr, Implies):
-        return to_core_logic_form(Or(Not(expr.left), expr.right), context, expand_all)
-    elif isinstance(expr, Iff):
-        return to_core_logic_form(And(Implies(expr.left, expr.right), Implies(expr.right, expr.left)), context, expand_all)
-    elif isinstance(expr, (Exists, Forall)):
-        return type(expr)(expr.var, to_core_logic_form(expr.body, context, expand_all))
-    elif isinstance(expr, ExistsUniq):
-        used_vars = {expr.var} | collect_vars(expr.body)[0] | collect_vars(expr.body)[1]
-        vardash = fresh_var(Var(expr.var.name + "'"), used_vars)
-        body_subst = substitute(expr.body, {expr.var: vardash})
-        expanded = Exists(expr.var, And(expr.body, Forall(vardash, Implies(body_subst, Symbol(context.equality.equal.name, [vardash, expr.var])))))
-        return to_core_logic_form(expanded, context, expand_all)
-    else:
-        raise Exception(f"Unexpected expr: {pretty_expr(expr)}")
-
 def fresh_var(var: Var, used: set[Var]) -> Var:
     if var in used:
         i = 0
@@ -339,46 +275,6 @@ def substitute(expr, mapping: dict[Term, Term], used_vars: set[Var] | None = Non
 
     return expr
 
-def to_nnf(expr):
-    if isinstance(expr, Symbol):
-        return expr
-    elif isinstance(expr, Not):
-        body = expr.body
-        if isinstance(body, Symbol):
-            return expr
-        elif isinstance(body, Not):
-            return to_nnf(body.body)
-        elif isinstance(body, And):
-            return Or(to_nnf(Not(body.left)), to_nnf(Not(body.right)))
-        elif isinstance(body, Or):
-            return And(to_nnf(Not(body.left)), to_nnf(Not(body.right)))
-        elif isinstance(body, Exists):
-            return Forall(body.var, to_nnf(Not(body.body)))
-        elif isinstance(body, Forall):
-            return Exists(body.var, to_nnf(Not(body.body)))
-        else:
-            raise Exception(f"Unexpected expr (Not): {pretty_expr(expr)}")
-    elif isinstance(expr, (And, Or)):
-        return type(expr)(to_nnf(expr.left), to_nnf(expr.right))
-    elif isinstance(expr, (Exists, Forall)):
-        return type(expr)(expr.var, to_nnf(expr.body))
-    else:
-        raise Exception(f"Unexpected expr: {pretty_expr(expr)}")
-
-def strip_quantifiers(expr) -> tuple[list[Forall | Exists], object]:
-    qs = []
-    body = expr
-    while isinstance(body, (Exists, Forall)):
-        qs.append(type(body)(body.var, None))
-        body = body.body
-    return qs, body
-
-def make_quantifiers(qs: list[Forall | Exists], body):
-    expr = body
-    for q in reversed(qs):
-        expr = type(q)(q.var, expr)
-    return expr
-
 def alpha_rename(expr, rename_map: dict[Var, Var]):
     if isinstance(expr, Symbol):
         new_args = [alpha_rename(a, rename_map) for a in expr.args]
@@ -403,142 +299,6 @@ def alpha_rename(expr, rename_map: dict[Var, Var]):
         return type(expr)(var, alpha_rename(expr.body, rename_map))
     else:
         return expr
-
-def rename_bound(expr, bound_vars: set[Var], used: set[Var]):
-    rename_map = {}
-    for v in bound_vars:
-        if v in used:
-            rename_map[v] = fresh_var(v, used)
-    return alpha_rename(expr, rename_map)
-
-def to_pnf(expr):
-    if isinstance(expr, Symbol):
-        return expr
-    elif isinstance(expr, Not):
-        return Not(to_pnf(expr.body))
-    elif isinstance(expr, And) or isinstance(expr, Or):
-        left_pnf = to_pnf(expr.left)
-        right_pnf = to_pnf(expr.right)
-        left_free, left_bound = collect_vars(left_pnf)
-        right_free, right_bound = collect_vars(right_pnf)
-        left_pnf = rename_bound(left_pnf, left_bound, right_free)
-        right_pnf = rename_bound(right_pnf, right_bound, left_free | left_bound)
-        qs_left, core_left = strip_quantifiers(left_pnf)
-        qs_right, core_right = strip_quantifiers(right_pnf)
-        core = type(expr)(core_left, core_right)
-        return make_quantifiers(qs_left + qs_right, core)
-    elif isinstance(expr, (Exists, Forall)):
-        return type(expr)(expr.var, to_pnf(expr.body))
-    else:
-        raise Exception(f"Unexpected expr: {pretty_expr(expr)}")
-
-def to_cnf(expr):
-    if isinstance(expr, Symbol):
-        return expr
-    elif isinstance(expr, Not):
-        return Not(to_cnf(expr.body))
-    elif isinstance(expr, And):
-        return And(to_cnf(expr.left), to_cnf(expr.right))
-    elif isinstance(expr, Or):
-        left_cnf = to_cnf(expr.left)
-        right_cnf = to_cnf(expr.right)
-        if isinstance(left_cnf, And):
-            return And(to_cnf(Or(left_cnf.left, right_cnf)), to_cnf(Or(left_cnf.right, right_cnf)))
-        elif isinstance(right_cnf, And):
-            return And(to_cnf(Or(left_cnf, right_cnf.left)), to_cnf(Or(left_cnf, right_cnf.right)))
-        else:
-            return Or(left_cnf, right_cnf)
-    elif isinstance(expr, (Exists, Forall)):
-        return type(expr)(expr.var, to_cnf(expr.body))
-    else:
-        raise Exception(f"Unexpected expr: {expr}")
-
-def simplify_op(expr):
-    if not isinstance(expr, (And, Or)):
-        raise Exception(f"Unexpected expr: {pretty_expr(expr)}")
-    parts = flatten_op(expr, type(expr))
-    simplified_parts = []
-    for p in parts:
-        if isinstance(p, BoolTrue):
-            if isinstance(expr, Or):
-                return BoolTrue()
-            else:
-                continue
-        elif isinstance(p, BoolFalse):
-            if isinstance(expr, And):
-                return BoolFalse()
-            else:
-                continue
-        else:
-            contradiction = False
-            skip = False
-            for q in simplified_parts:
-                if alpha_equiv(p, Not(q)) or alpha_equiv(Not(p), q):
-                    contradiction = True
-                    break
-                if alpha_equiv(p, q):
-                    skip = True
-                    break
-            if contradiction:
-                if isinstance(expr, And):
-                    return BoolFalse()
-                else:
-                    return BoolTrue()
-            if skip:
-                continue
-            simplified_parts.append(p)
-    if len(simplified_parts) == 0:
-        if isinstance(expr, And):
-            return BoolTrue()
-        else:
-            return BoolFalse()
-    elif len(simplified_parts) == 1:
-        return simplified_parts[0]
-    else:
-        result = simplified_parts[0]
-        for part in simplified_parts[1:]:
-            result = type(expr)(result, part)
-        return result
-
-def simplify(expr):
-    if isinstance(expr, (Symbol, Not)):
-        return expr
-    elif isinstance(expr, And):
-        left_s = simplify(expr.left)
-        right_s = simplify(expr.right)
-        return simplify_op(And(left_s, right_s))
-    elif isinstance(expr, Or):
-        left_s = simplify(expr.left)
-        right_s = simplify(expr.right)
-        return simplify_op(Or(left_s, right_s))
-    elif isinstance(expr, (Exists, Forall)):
-        body_simplified = simplify(expr.body)
-        if expr.var not in collect_vars(body_simplified)[0]:
-            return body_simplified
-        else:
-            return type(expr)(expr.var, body_simplified)
-    else:
-        raise Exception(f"Unexpected expr: {expr}")
-
-def to_canonical_form(expr, context: Context, expand_all: bool = False):
-    expr_norm = to_core_logic_form(expr, context, expand_all)
-    expr_norm = to_nnf(expr_norm)
-    expr_norm = to_pnf(expr_norm)
-    expr_norm = to_cnf(expr_norm)
-    expr_norm = simplify(expr_norm)
-    return expr_norm
-
-def logic_equiv(expr1, expr2, context: Context, expand_all: bool = False) -> bool:
-    if isinstance(expr1, Bottom) and isinstance(expr2, Bottom):
-        return True
-    elif isinstance(expr1, Bottom) and not isinstance(expr2, Bottom):
-        return False
-    elif not isinstance(expr1, Bottom) and isinstance(expr2, Bottom):
-        return False
-    else:
-        expr1_norm = to_canonical_form(expr1, context, expand_all)
-        expr2_norm = to_canonical_form(expr2, context, expand_all)
-        return alpha_equiv(expr1_norm, expr2_norm)
 
 def make_tree(expr, prefix: str = "", is_root: bool = True, is_last: bool = True) -> str:
     print_prefix = prefix + ("" if is_root else ("└─" if is_last else "├─"))
@@ -565,8 +325,6 @@ def make_tree(expr, prefix: str = "", is_root: bool = True, is_last: bool = True
     elif isinstance(expr, (Exists, Forall, ExistsUniq)):
         body_str = make_tree(expr.body, child_pref, False, True)
         return f"{print_prefix}{type(expr).__name__}({expr.var.name})\n{body_str}"
-    elif isinstance(expr, (BoolTrue, BoolFalse)):
-        return type(expr).__name__
     else:
         raise Exception(f"Unexpected expr: {expr}")
 
