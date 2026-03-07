35ac72be406beefdd8edb15283b9427ab43bb0b2
check-your-proofs <233064824+check-your-proofs@users.noreply.github.com>
2026-03-07 18:02:15 +0900

    Fix name collision of tokens in split()

--- proofsrc/splitter.py
+++ proofsrc/splitter.py
@@ -4,25 +4,25 @@ from ast_types import DeclarationUnit
 
 import hashlib
 
-def split(file: str, tokens: list[Token], source: str) -> list[DeclarationUnit]:
-    stream = TokenStream(tokens)
+def split(file: str, file_tokens: list[Token], source: str) -> list[DeclarationUnit]:
+    stream = TokenStream(file_tokens)
     units: list[DeclarationUnit] = []
     while True:
         start_token = stream.peek()
         if start_token.type == "EOF":
             break
         stream.consume(start_token.type)
-        tokens = [start_token] + get_tokens_until_next(stream)
-        last_token = tokens[-1]
-        tokens.append(Token("EOF", "", last_token.file, last_token.pos + len(last_token.value), last_token.line, last_token.column + len(last_token.value), last_token.line, last_token.column + len(last_token.value)))
-        for index, token in enumerate(tokens):
+        unit_tokens = [start_token] + get_tokens_until_next(stream)
+        last_token = unit_tokens[-1]
+        unit_tokens.append(Token("EOF", "", last_token.file, last_token.pos + len(last_token.value), last_token.line, last_token.column + len(last_token.value), last_token.line, last_token.column + len(last_token.value)))
+        for index, token in enumerate(unit_tokens):
             token.index = index
-        start = tokens[0].pos
-        end = tokens[-1].pos
+        start = unit_tokens[0].pos
+        end = unit_tokens[-1].pos
         raw_text = source[start:end]
         normalized_text = raw_text.replace("\r\n", "\n")
         hash = hashlib.md5(normalized_text.encode()).hexdigest()
-        unit = DeclarationUnit(file=file, tokens=tokens, hash=hash)
+        unit = DeclarationUnit(file=file, tokens=unit_tokens, hash=hash)
         units.append(unit)
     return units
 
